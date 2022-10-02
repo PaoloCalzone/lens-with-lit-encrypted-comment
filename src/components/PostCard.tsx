fca28a16a4b9fac4fe4303e3eb293d4a6cafe4f4
paolocalzone <paolocalzone@protonmail.com>
2022-10-02 10:02:05 +0200

    Style post Card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -13,12 +13,24 @@ interface Props {
 const PostCard: FC<Props> = ({ key, id, handle, content, postTimestamp, avatarURL }) => {
 	return (
 		<div className="group relative clickable-card rounded-lg focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500">
-			<p className="mt-2 block text-sm text-gray-500">{content}</p>
-			<div className="block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 overflow-hidden relative group-hover:opacity-75">
-				{avatarURL && <Image src={avatarURL} alt="event image" layout="fill" />}
+			<div className="flex justify-between pb-4 space-x-1.5">
+				<div className="flex items-center space-x-3">
+					{avatarURL && (
+						<Image
+							src={avatarURL}
+							alt="event image"
+							className="rounded-full border"
+							loading="lazy"
+							width={40}
+							height={40}
+						/>
+					)}
+
+					<p className="block text-base font-medium text-gray-900">{handle}</p>
+				</div>
 			</div>
+			<p className="mt-2 block text-sm text-gray-500">{content}</p>
 			<p className="mt-2 block text-sm text-gray-500">{formatTimestamp(postTimestamp)}</p>
-			<p className="block text-base font-medium text-gray-900">{handle}</p>
 		</div>
 	)
 }
